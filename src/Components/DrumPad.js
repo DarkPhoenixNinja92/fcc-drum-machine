688d7f6ddd2cea76ac27974b9d5cebab7a980e4a
Shaun <scpour@gmail.com>
2022-09-28 09:49:00 -0400

    keypad and sound setup

---
+++ src/Components/DrumPad.js
@@ -0,0 +1,13 @@
+import Keypad from "./Keypad";
+
+const DrumPad = () => {
+    return (
+        <div id="drum-machine">
+            <div id="display">
+            </div>
+            <Keypad />
+        </div>
+    )
+}
+
+export default DrumPad;
---
+++ src/Components/Keypad.js
@@ -0,0 +1,35 @@
+const Keypad = () => {
+    return(
+        <div className="keypad">
+            <button className="drum-pad" id="heater-1">Q
+                <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"/>
+                </button>
+                <button className="drum-pad" id="heater-2">W
+                <audio className="clip" id="W" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"/>
+                </button>
+                <button className="drum-pad" id="heater-3">E
+                <audio className="clip" id="E" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"/>
+                </button>
+                <button className="drum-pad" id="heater-4">A
+                <audio className="clip" id="A" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"/>
+                </button>
+                <button className="drum-pad" id="clap">S
+                <audio className="clip" id="S" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"/>
+                </button>
+                <button className="drum-pad" id="open-hh">D
+                <audio className="clip" id="D" src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"/>
+                </button>
+                <button className="drum-pad" id="kick-n-hat">Z
+                <audio className="clip" id="Z" src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"/>
+                </button>
+                <button className="drum-pad" id="kick">X
+                <audio className="clip" id="X" src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"/>
+                </button>
+                <button className="drum-pad" id="closed-hh">C
+                <audio className="clip" id="C" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"/>
+                </button>
+        </div>
+    )
+}
+
+export default Keypad;
